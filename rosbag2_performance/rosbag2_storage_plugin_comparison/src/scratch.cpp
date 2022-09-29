2a656683cab0f792c599a927c64b3435c70b0c12
James Smith <james@foxglove.dev>
2022-09-29 10:10:12 +1000

    much more sophisticated

--- rosbag2_performance/rosbag2_storage_plugin_comparison/src/scratch.cpp
+++ rosbag2_performance/rosbag2_storage_plugin_comparison/src/scratch.cpp
@@ -1,9 +1,14 @@
 #include <malloc.h>
+#include <random>
+#include <climits>
+#include <algorithm>
+#include <functional>
 #include <vector>
 #include <iostream>
 #include <unordered_map>
 
 #include "rosbag2_cpp/writer.hpp"
+#include "rosbag2_storage/ros_helper.hpp"
 
 #ifdef _WIN32
 // This is necessary because of a bug in yaml-cpp's cmake
@@ -18,25 +23,109 @@
 # pragma warning(pop)
 #endif
 
-
+using RandomEngine = std::independent_bits_engine<std::default_random_engine, CHAR_BIT, unsigned char>;
 using hrc = std::chrono::high_resolution_clock;
 using Batch = std::vector<std::shared_ptr<const rosbag2_storage::SerializedBagMessage>>;
 
 struct Config {
-    std::string storage_id = "";
-    size_t batch_message_count = 100;
+    std::string storage_id = "mcap";
+    size_t batch_num_messages = 10;
+    size_t repeat_message_count = 1000;
+
+    struct TopicConfig {
+        std::string name;
+        size_t message_size;
+    };
+    std::vector<TopicConfig> topics = { {"/large", 1000000 }, {"/small", 1000} };
 
     std::unordered_map<std::string, std::string> plugin_config;
 };
 
+namespace YAML {
+template<>
+struct convert<Config> {
+  static Node encode(const Config& rhs) {
+    Node node;
+    node["storage_id"] = rhs.storage_id;
+    node["batch_num_messages"] = rhs.batch_num_messages;
+    node["repeat_message_count"] = rhs.repeat_message_count;
+    Node topic_configs_node;
+    for (const auto& topic_config : rhs.topics) {
+        Node topic_config_node;
+        topic_config_node["name"] = topic_config.name;
+        topic_config_node["message_size"] = topic_config.message_size;
+        topic_configs_node.push_back(topic_config_node);
+    }
+    node["topics"] = topic_configs_node;
+    Node plugin_config_node;
+    for (const auto& [k, v] : rhs.plugin_config) {
+        plugin_config_node[k] = v;
+    }
+    node["plugin_config"] = plugin_config_node;
+    return node;
+  }
 
-std::vector<rosbag2_storage::TopicMetadata> generate_topics() {
+  static bool decode(const Node& node, Config& rhs) {
+    optional_assign<std::string>(node, "storage_id", rhs.storage_id);
+    optional_assign<size_t>(node, "batch_num_messages", rhs.batch_num_messages);
+    optional_assign<size_t>(node, "repeat_message_count", rhs.repeat_message_count);
+    if (node["topics"]) {
+        rhs.topics.clear();
+        auto topics_node = node["topics"];
+        for (auto it = topics_node.begin(); it != topics_node.end(); ++it) {
+            Config::TopicConfig topic;
+            topic.name = (*it)["name"].as<std::string>();
+            topic.message_size = (*it)["message_size"].as<size_t>();
+            rhs.topics.push_back(topic);
+        }
+    }
+    optional_assign<std::unordered_map<std::string, std::string>>(node, "plugin_config", rhs.plugin_config);
+    return true;
+  }
+};
+}
+
+
+std::vector<rosbag2_storage::TopicMetadata> generate_topics(const Config& config) {
     std::vector<rosbag2_storage::TopicMetadata> topics;
+    for (const auto& topic : config.topics) {
+        rosbag2_storage::TopicMetadata metadata;
+        metadata.name = topic.name;
+        // The topic type doesn't matter here - we're not doing any serialization,
+        // just throwing random bytes into the serialized message.
+        metadata.type = "std_msgs/String";
+        metadata.serialization_format = "cdr";
+        metadata.offered_qos_profiles = "";
+        topics.push_back(metadata);
+    }
     return topics;
 }
 
-std::vector<Batch> generate_messages() {
-    std::vector<std::vector<std::shared_ptr<const rosbag2_storage::SerializedBagMessage>>> out;
+std::shared_ptr<rcutils_uint8_array_t> random_uint8_array(size_t size, RandomEngine& engine) {
+    std::vector<unsigned char> data(size);
+    std::generate(begin(data), end(data), std::ref(engine));
+    return rosbag2_storage::make_serialized_message(data.data(), data.size());
+}
+
+std::vector<Batch> generate_messages(Config config) {
+    std::vector<Batch> out;
+    Batch currentBatch;
+    RandomEngine engine;
+    for (size_t i = 0; i < config.repeat_message_count; ++i) {
+        for (const auto& topic_config: config.topics) {
+            auto msg = std::make_shared<rosbag2_storage::SerializedBagMessage>();
+            msg->topic_name = topic_config.name;
+            msg->serialized_data = random_uint8_array(topic_config.message_size, engine);
+            currentBatch.push_back(msg);
+            if (currentBatch.size() >= config.batch_num_messages) {
+                out.emplace_back(std::move(currentBatch));
+                currentBatch = {};
+            }
+        }
+    }
+    if (currentBatch.size() != 0) {
+        out.emplace_back(std::move(currentBatch));
+    }
     return out;
 }
 
@@ -84,22 +173,23 @@ struct WriteStat {
 };
 
 
-int main(int argc, const char* argv[]) {
-    if (argc < 2) {
-        std::cerr << "please supply an options file as input" << std::endl;
-        return 1;
-    }
-    auto topics = generate_topics();
-    auto messages = generate_messages();
+int main(int, const char**) {
+    Config config;
+    std::cerr << "generating topics" << std::endl;
+    auto topics = generate_topics(config);
+    std::cerr << "generating " << config.repeat_message_count * config.topics.size() << " messages" << std::endl;
+    auto messages = generate_messages(config);
 
+    std::cerr << "setting up writer" << std::endl;
     rosbag2_storage::StorageFactory factory;
     rosbag2_storage::StorageOptions options;
-    options.uri = "out.mcap";
-    options.storage_id = "mcap";
+    options.uri = "out";
+    options.storage_id = config.storage_id;
 
     std::vector<WriteStat> writeStats;
     writeStats.reserve(messages.size());
     std::chrono::high_resolution_clock::time_point close_start_time;
+    std::cerr << "starting writes" << std::endl;
     {
         auto writer = factory.open_read_write(options);
 
@@ -122,5 +212,20 @@ int main(int argc, const char* argv[]) {
         // writer destructor closes the output file, so we time that too.
     }
     auto close_duration = hrc::now() - close_start_time;
+
+    std::cout << "sqc,num_bytes,num_msgs,write_ns,arena_bytes,in_use_bytes,mmap_bytes,close_ns" << std::endl;
+    for (const auto& stat : writeStats) {
+        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stat.writeDuration).count();
+        std::cout <<
+            stat.sqc << "," <<
+            stat.bytesWritten << "," <<
+            stat.numMsgs << "," <<
+            duration << "," <<
+            stat.arenaBytes << "," <<
+            stat.inUseBytes << "," <<
+            stat.mmapBytes << "," <<
+            std::endl;
+    }
+    std::cout << ",,,,,,," << std::chrono::duration_cast<std::chrono::nanoseconds>(close_duration).count() << std::endl;
     return 0;
 }
